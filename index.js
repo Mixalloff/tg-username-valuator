48c9603c68d7cff0599ca6966ca9f55442859b31
Mixalloff <m.mikhalev@gateline.ru>
2024-06-14 13:28:41 +0300

    Root dir changed in tsconfig

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TgUsernameValuator = void 0;
+var TgUsernameValuator_1 = require("./src/TgUsernameValuator");
+Object.defineProperty(exports, "TgUsernameValuator", { enumerable: true, get: function () { return TgUsernameValuator_1.TgUsernameValuator; } });
